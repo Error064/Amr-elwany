54db0a2688872f454c7acf6664472c1fba9425d6
Error064 <102310611+Error064@users.noreply.github.com>
2022-03-24 17:35:27 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,113 @@
+<center>
+<head>
+    <meta charset= utf-8 >
+    <meta http-equiv= X-UA-Compatible  content= IE=edge >
+    <html>
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3782750975369789"
+     crossorigin="anonymous"></script>
+    <title>Scripts</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300&display=swap" rel="stylesheet">
+    <meta name= viewport  content= width=device-width, initial-scale=1 >
+     <link rel= stylesheet  type= text/css  media= screen  href= fre.css >
+    </head>
+<body>
+<div class="nav">
+</div>
+<div class="menu">
+<h4>AMR ELWANY -- AHMED RAGAB</h4>
+<br>
+<a href="Ccam.php"><button>Cccam Server</button></a>
+<br><br>
+<a href="IPTV.php"><button>IPTV FILES</button></a>
+<br><br>
+<a href="matches.php"><button>جدول مباريات اليوم</button></a>
+<br><br>
+<a href="channel.php"><button>القنوات الناقلة اليوم</button></a>
+<br><br>
+<a href="bein sport.html"><button>مباريات جارية حاليا</button></a>
+<br><br>
+<a href="Xtream code.php"><button>Xtream Iptv</button></a>
+<br><br>
+<a href="Apps.php"><button>برامج لمشاهدة المباريات</button></a>
+<br><br>
+<a href="Other.php"><button>Other</button></a>
+<br><br>
+<a style="color:#fff;text-decoration:none" href="https://t.me/FREE_NET_5_G" >Telegram Channel</a>
+</div>
+      
+                                          
+                          
+</body>
+</body>
+</center>
+
+<style>
+.menu{
+background:rgba(50,50,50,.4);
+padding:0px;
+width:350px;
+height:650px;
+margin:0px;
+border-radius:12px 12px 12px 12px;
+box-shadow:0px 0px 0px 0px;
+} 
+h4{
+margin-top:0px;
+width:310px;
+padding:20px;
+background:#0063B1;
+color:#fff;
+border-radius:12px 12px 0px 0px;
+} 
+.nav{
+background:none;
+padding:5px;
+color:#fff;
+}
+button:hover{
+background:#000000;
+border:1px solid red;
+} 
+body{
+font-family:  Tajawal , sans-serif;
+margin:0px;
+padding:0px;
+width:100%;
+background-size:cover;
+background:url(IMG_20220101_182447_312.jpg);
+
+}
+button{
+font-family:  Tajawal , sans-serif;
+text-decoration:none;
+color:yellow;
+background:#000000;
+font-size:15px;
+padding:12px;
+width:90%;
+border:1px solid red;
+border-radius:90px;
+
+
+} 
+p{
+color:#fff;
+margin-left:120px;
+font-size:15px;
+}
+video{
+width:90px;
+border-radius:12px;
+border:1px solid #FFFFFF;
+}
+img{
+width:50px;
+height:50px;
+margin-left:70px;
+border-radius:90px;
+
+} 
+</style>
+</div>
